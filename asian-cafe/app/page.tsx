d05a70caba4402f9f0e19afca2f31e52fe3c561d
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-01-01 16:53:22 -0500

    header

--- asian-cafe/app/page.tsx
+++ asian-cafe/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import ACLogo from './ui/ac-logo';
 import NavLinks from './ui/navlinks';
+import Header from './ui/header';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[200px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-4 row-start-1 items-center justify-center">
-        <ACLogo width={100} height={100}/>
-        <NavLinks />
+        <Header />
 
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
---
+++ asian-cafe/app/ui/header.tsx
@@ -0,0 +1,11 @@
+import ACLogo from './ac-logo';
+import NavLinks from './navlinks';
+
+export default function Header() {
+  return (
+    <div className="flex flex-col gap-4 row-start-1 items-center justify-center">
+      <ACLogo width={100} height={100}/>
+      <NavLinks />
+    </div>
+  );
+}
