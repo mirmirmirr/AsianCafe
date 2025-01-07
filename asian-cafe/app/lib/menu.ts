2121f1db19879cc2f1f28bceabd7704a66dbfe34
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-01-07 11:47:11 -0500

    new dbs setup

--- asian-cafe/app/lib/menu.ts
+++ asian-cafe/app/lib/menu.ts
@@ -13,8 +13,19 @@ export async function fetchSectionItems() {
 
   try {
     const data = await client.sql`
-      SELECT *
-      FROM menu;
+      SELECT 
+        ms.title AS section_title, 
+        ms.description AS section_description,
+        mss.title AS subsection_title,
+        mss.description AS subsection_description,
+        mi.menu_item_name, 
+        mi.price, 
+        mi.spicy, 
+        mi.description AS item_description
+      FROM menu_item mi
+        LEFT JOIN menu_section ms USING (menu_section_id)
+        LEFT JOIN menu_subsection mss USING (menu_subsection_id)
+        ORDER BY ms.menu_section_id, mss.menu_subsection_id, mi.menu_item_id;
     `;
 
     return data.rows;
@@ -24,14 +35,24 @@ export async function fetchSectionItems() {
   }
 }
 
-export async function fetchExtraOptions(selectedCat) {
+export async function fetchExtraOptions(itemID) {
   const client = await getClient();
 
   try {
     const data = await client.sql`
-      SELECT option, price, priority, "getQuantity"
-      FROM extras
-      WHERE category = ${selectedCat}
+      WITH addon_codes AS (
+        SELECT addon_category_id
+        FROM menu_addon
+        WHERE menu_item_id = ${itemID}
+      )
+      SELECT
+        ac.title AS category_title,
+        a.addon_name,
+        a.price,
+        a.get_quantity
+      FROM addon a
+        JOIN addon_category ac USING (addon_category_id)
+        JOIN addon_codes ad ON a.addon_category_id = ad.addon_category_id;
     `;
 
     return data.rows;
