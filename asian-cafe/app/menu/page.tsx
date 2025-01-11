69c998e3f10c0514382d0e23f81c03a2091667fc
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-01-11 13:14:36 -0500

    header offset added

--- asian-cafe/app/menu/page.tsx
+++ asian-cafe/app/menu/page.tsx
@@ -4,37 +4,23 @@ import { Section, SectionMobile } from "../ui/menu/menu-items";
 import api from '@/app/lib/axios';
 import Order from '@/app/ui/menu/order';
 import OrderSummary from '../ui/menu/order-summary';
+import CategoryLinks from '../ui/menu/category-links';
 
 export default async function Page() {
   const response = await api.get('/api/menu');
   const data = response.data;
 
   const sections = data['menu'];
-  // console.log(sections[0].section);
-
+  
   return (
     <div className="grid grid-rows-[240px_1fr_20px] min-h-screen gap-8">
       <main className="row-start-2 md:grid md:grid-cols-[1fr_350px] lg:grid-cols-[300px_1fr_350px] gap-x-8">
-        <div className='hidden lg:block border-2 border-black p-4 h-[350px] col-start-1 sticky top-[140px]'>
-          <h1 className='text-[20px] font-[600]'>Categories</h1>
-          <ol>
-            {sections.map((category, index) => (
-                <li key={index}>
-                  <a
-                    href={`#${category.section.replace(/\s+/g, "-").toLowerCase()}`}
-                    className="hover:underline"
-                  >
-                    {category.section}
-                  </a>
-                </li>
-              ))}
-          </ol>
-        </div>
+        <CategoryLinks sections={sections} />
         <div className='md:col-start-1 lg:col-start-2'>
           {sections.map((category, index) => (
             <div
               key={index}
-              id={category.section.replace(/\s+/g, "-").toLowerCase()}
+              id={category.id}
             >
               <div className='hidden md:block'> <Section section={category}/> </div>
               <div className='md:hidden'> <SectionMobile section={category} /> </div>
