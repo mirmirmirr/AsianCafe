9cc56e582ae7c2bc4d1f21707db82e441c383b87
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-02-19 09:33:04 -0500

    editing appending slash comamnd

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path("ping", views.ping, name="ping"),
-    path("menu/", views.get_menu, name="menu"),
-    path("extras/<str:itemID>/", views.get_extras, name="extras"),
-    path("get_order/", views.get_order, name="get_order"),
-    path("add_order_item/", views.add_order_item, name="add_order_item"),
-    path("order_item/<int:order_item_id>/", views.order_item, name="order_item"),
-    path("checkout/", views.checkout, name="checkout"),
+    path("menu", views.get_menu, name="menu"),
+    path("extras/<str:itemID>", views.get_extras, name="extras"),
+    path("get_order", views.get_order, name="get_order"),
+    path("add_order_item", views.add_order_item, name="add_order_item"),
+    path("order_item/<int:order_item_id>", views.order_item, name="order_item"),
+    path("checkout", views.checkout, name="checkout"),
 ]
