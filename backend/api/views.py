5b7b055230726e550ec6acc9deaa89f0a816ab41
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-03-03 13:24:57 -0500

    Update views with new dbs structure

--- backend/api/views.py
+++ backend/api/views.py
@@ -19,11 +19,17 @@ def get_menu(request):
         SELECT 
           mi.menu_item_id AS id,
           ms.menu_section_id AS section_id,
-          ms.title AS section_title, 
-          ms.description AS section_description,
-          mss.title AS subsection_title,
-          mss.description AS subsection_description,
-          mic.menu_item_code as item_code,
+          ms.section_title AS section_title, 
+          CASE 
+            WHEN ms.subsection_title IS NULL THEN ms.description 
+            ELSE NULL 
+          END AS section_description,
+          ms.subsection_title AS subsection_title,
+          CASE 
+            WHEN ms.subsection_title IS NOT NULL THEN ms.description 
+            ELSE NULL 
+          END AS subsection_description,
+          mic.menu_item_code AS item_code,
           mi.menu_item_name, 
           mi.price, 
           mi.spicy, 
@@ -31,8 +37,7 @@ def get_menu(request):
         FROM menu_item mi
           LEFT JOIN menu_item_code mic USING (menu_item_id)
           LEFT JOIN menu_section ms USING (menu_section_id)
-          LEFT JOIN menu_subsection mss USING (menu_subsection_id)
-          ORDER BY ms.menu_section_id, mss.menu_subsection_id, mi.menu_item_id;
+        ORDER BY ms.menu_section_id, mi.menu_item_id;
     """)
     rows = cursor.fetchall()
     column_names = [desc[0] for desc in cursor.description]
@@ -45,9 +50,9 @@ def get_menu(request):
 def get_extras(request, itemID):
   with connection.cursor() as cursor:
     cursor.execute ("""
-      WITH addon_codes AS (
-        SELECT addon_category_id
-        FROM menu_addon 
+      WITH allowed_addons AS (
+        SELECT addon_id
+        FROM expanded_menu_addon 
         WHERE menu_item_id = %s
       )
       SELECT
@@ -58,7 +63,7 @@ def get_extras(request, itemID):
         a.get_quantity
       FROM addon a
         JOIN addon_category ac USING (addon_category_id)
-        JOIN addon_codes ad ON a.addon_category_id = ad.addon_category_id
+        JOIN allowed_addons aa USING (addon_id)
       ORDER BY a.addon_id;
       """, [itemID])
     rows = cursor.fetchall()
