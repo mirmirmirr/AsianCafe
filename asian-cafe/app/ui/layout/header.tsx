cb5af10d1bdad6f185d391d9e561a0098ad5ec21
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-03-03 13:47:27 -0500

    new green background

--- asian-cafe/app/ui/layout/header.tsx
+++ asian-cafe/app/ui/layout/header.tsx
@@ -1,16 +1,3 @@
-// import ACLogo from './ac-logo';
-// import NavLinks from './navlinks';
-
-// export default function Header() {
-//   return (
-//     <div className="flex flex-col gap-4 row-start-1 items-center justify-center">
-//       <ACLogo width={100} />
-//       <NavLinks />
-//     </div>
-//   );
-// }
-
-
 'use client';
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
@@ -22,7 +9,7 @@ export default function Header() {
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.scrollY > 50); // Change state when scrolled past 50px
+      setIsScrolled(window.scrollY > 50);
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -34,7 +21,7 @@ export default function Header() {
   return (
     <div className='flex items-center justify-center'>
       <motion.div
-        className={`fixed top-[70px] md:top-[40px] w-[95vw] z-10 bg-lightgreen flex border-b-2 border-black p-8 pt-4`}
+        className={`fixed top-[70px] md:top-[40px] w-[95vw] z-10 bg-[#DEEBD1] flex border-b-2 border-black p-8 pt-4`}
         initial={false}
         animate={{
         flexDirection: isScrolled ? 'row' : 'column',
